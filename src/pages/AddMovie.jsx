65eb6f6e3f2b235775c8180b58456976c611af20
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-12-05 12:53:47 +0600

    add movie form added successfully

--- src/pages/AddMovie.jsx
+++ src/pages/AddMovie.jsx
@@ -1,8 +1,177 @@
+import { useState } from "react";
+import { Rating } from "react-simple-star-rating";
 
 const AddMovie = () => {
+
+    const [selectedGenres, setSelectedGenres] = useState([]);
+
+    // console.log(selectedGenres)
+
+    const handleGenreChange = (event) => {
+      const options = event.target.selectedOptions; // Get selected options
+      const selectedValues = Array.from(options).map((option) => option.value); // Create an array of selected values
+      setSelectedGenres(selectedValues); // Update state with selected genres
+    };
+
+
+  const handleAddMovie = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    
+
+    const movieposter = form.movieposter.value;
+    const movietitle = form.movietitle.value;
+    const genres = selectedGenres;
+
+    console.log(movietitle,movieposter,genres);
+
+
+
+  };
   return (
-    <div>
-      <p>Add Movies</p>
+    <div className="mt-6 lg:mt-10">
+      <div className="hero  max-w-7xl mx-auto min-h-screen bg-gradient-to-b from-background to-primary">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold font-Cinzel">
+              Add Movie
+            </h1>
+            <p className="py-4 w-4/5 mx-auto text-xl">
+              Add Your Favorite Movies: Easily submit movie details to
+              CineVerse, helping others discover new films, share your
+              recommendations, and contribute to an ever-growing collection of
+              cinematic experiences.
+            </p>
+          </div>
+          <div className="card w-full shrink-0">
+            <form
+              onSubmit={handleAddMovie}
+              className="card-body space-y-3 text-background"
+            >
+              <div className="lg:flex gap-5">
+                <div className="form-control lg:w-1/2">
+                  <label className="label">
+                    <span className="label-text text-text">Movie Poster</span>
+                  </label>
+                  <input
+                    type="url"
+                    name="movieposter"
+                    placeholder="Enter Movie Poster URL"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control lg:w-1/2">
+                  <label className="label">
+                    <span className="label-text text-text">Movie Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Movie Title"
+                    name="movietitle"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="lg:flex gap-5">
+                <div className="form-control lg:w-1/2">
+                  <label className="label">
+                    <span className="label-text text-text">Genre</span>
+                  </label>
+                  <select
+                    className="select  w-full input "
+                    multiple
+                    value={selectedGenres}
+                    onChange={handleGenreChange}
+                  >
+                    <option disabled selected>
+                      Types of Movies
+                    </option>
+                    <option value="comedy">Comedy</option>
+                    <option value="drama">Drama</option>
+                    <option value="horor">Horor</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="action">Action</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="adventure">Adventure</option>
+                  </select>
+                </div>
+                <div className="form-control lg:w-1/2">
+                  <label className="label">
+                    <span className="label-text text-text">Duration</span>
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Enter Movie Duration"
+                    name="duration"
+                    className="input input-bordered"
+                    min='60'
+                    required
+                  />
+                </div>
+              </div>
+              <div className="lg:flex lg:items-center  lg:justify-center  gap-5">
+                <div className="form-control lg:w-1/2">
+                  <label className="label">
+                    <span className="label-text text-text">Release Year</span>
+                  </label>
+                  <input
+                    type="date"
+                    name="releaseyear"
+                    placeholder="Enter Relese Year"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className=" form-control flex  justify-center    lg:w-1/2  ">
+                  <label className="label">
+                    <span className="label-text text-text">Ratings</span>
+                  </label>
+                  <div
+                    style={{
+                      direction: "ltr",
+                      fontFamily: "sans-serif",
+                      touchAction: "none",
+                      border: "1px solid white",
+                    }}
+                  >
+                    <Rating
+                      onClick={function noRefCheck() {}}
+                      showTooltip
+                      tooltipArray={[
+                        "Terrible",
+                        "Bad",
+                        "Average",
+                        "Great",
+                        "Prefect",
+                      ]}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-text">Movie Summary</span>
+                  </label>
+                  <textarea
+                    placeholder="Enter Movie Summary"
+                    name="summary"
+                    className="textarea textarea-bordered textarea-lg w-full lg:max-w-7xl"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn  bg-secondary text-text hover:text-background text-xl font-semibold">
+                  Add Movie
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
